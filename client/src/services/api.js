eb84f7b5b919d5c6f869f49ee4c9562417f0cdb4
NeomiA1 <neomianikin123@gmail.com>
2026-05-04 13:15:21 +0300

    test

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,46 @@
-const API_BASE_URL = "http://localhost:5030/api";
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-export default API_BASE_URL;
+namespace RewearApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReact", policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowReact");
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseStaticFiles();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
