705029acb97249b7e92ea0361365132dce365900
NeomiA1 <neomianikin123@gmail.com>
2026-05-16 12:45:40 +0300

    Connect private register to Azure API

--- client/src/pages/auth/RegisterPrivatePage.jsx
+++ client/src/pages/auth/RegisterPrivatePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
+import { registerUser } from "../../services/UserService";
 
 export default function RegisterPrivatePage() {
 
